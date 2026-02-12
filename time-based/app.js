d70f15df8032c88758a4299ab033a6766425deae
Jessica A. Rodriguez <44589009+jac307@users.noreply.github.com>
2026-02-11 19:28:48 -0500

    Update app.js

--- time-based/app.js
+++ time-based/app.js
@@ -25,149 +25,136 @@ const App = {
             // 2
 
             {
-                title: "What I Remember",
-                author: "Sandhya Acharya",
-                description: `I’ve always had a terrible memory. This short documentary grows out of that feeling where 
-                            I try to piece together what I remember from my childhood, my parents, and the stories that 
-                            shaped me. I talk about my parents as complicated people, and how those memories shifted as 
-                            I got older. This film blends reflection with moments of humour, showing how laughter can exist 
-                            alongside grief. I also speak of my dad’s passing as a part of the ongoing way I remember him 
-                            and carry him with me.`,
+                title: "A not so long break",
+                author: "Austin Frise",
+                description: `What I was trying to get across with this project was just how fleeting the breaks we 
+                            have as students are. It starts out looking at the monitor of one of the computers in 
+                            the media lab, and then I have a bunch of transitional photographs that are from the 
+                            perspective of the main character walking to where they are taking a break. The catch is 
+                            that the amount of time spent on each photo is longer for the walking and transitional 
+                            photographs.`,
                 xButton: "68%",
                 yButton: "5%",
                 wButton: "25%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/02.png"
             },
 
             // 3
             {
-                title: "The Utopia Daughter: A Distance of 11,400km",
-                author: "Xuezi Jin (Eloise)",
-                description: `This autobiographical documentary explores how distance redefines intimacy. As a22-year-old
-                            woman raised in an East Asian household, I examine my anxious
-                            attachment style to reveal an absence in my upbringing, parental favoritismandemotional
-                            estrangement within my own family. The film spans 11,400 kilometers
-                            between Shanghai and Hamilton, documenting my journey from being a "spectator"in my birth
-                            family to finding belonging within my chosen family of close friends. Byrecognizing the
-                            seasonal nature of all relationships, I arrive at a reconciliation withpain: accepting the
-                            impermanence of connection while preserving a fragile yet
-                            precious "Utopian" bond with my parents, one that can only exist across distance.`,
+                title: "Noisy Places",
+                author: "Caidence Amartey.",
+                description: `This photo film is meant to display peaceful solitude. The title of the project works 
+                            to affirm that solitude can be found anywhere. The use of cowboy to close up shots was 
+                            intended to ensure all photos generate a sense of distant intimacy. Viewers are in the 
+                            space with the subject yet still remain far enough so as to not disturb them. The subject 
+                            is in their own serene world. Overexposing the lighting added to the effect of serenity. 
+                            It brightens the otherwise poorly lit library and isolates the subject without making the 
+                            subject feel isolated.`,
                 xButton: "20%",
                 yButton: "13%",
                 wButton: "40%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/03.png"
             },
 
             // 4
             {
-                title: "Life is like a bar of soap",
-                author: "Yuna Liu",
-                description: `Each summer, when I return home, my mother places a new bar of soap beside the sink. 
-                            By the time I leave again, it has quietly worn away. This simple ritual became the 
-                            starting point for an autobiographical film about time, memory, and distance.
-                            Through personal archives, voice over, and music, the work reflects on the experience 
-                            of growing up and moving away from home. The soap becomes a fragile clock that measures 
-                            the slow rhythm of leaving and returning, presence and absence. By focusing on an ordinary 
-                            object, the film invites viewers to consider how small, unnoticed moments hold emotional 
-                            weight, and how memories continue to change as we move forward.
-`,
+                title: "Signs of Presence",
+                author: "Xiaoqian Wen",
+                description: `I focus on school spaces where there are signs of student activity but no students appear. 
+                            Through small details in these corners, I try to show that students have been there. The 
+                            series starts with sunlight from a low angle, and the light slowly fades into darkness. 
+                            I mainly choose natural elements, metal, and walls, because they are common materials on 
+                            campus and create a real school feeling.`,
                 xButton: "64%",
                 yButton: "13%",
                 wButton: "30%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/04.png"
             },
 
 
             // 5
             {
-                title: "Through Static and Memory",
-                author: "Tyler Hooper",
-                description: `Project description: This autobiographical documentary explores childhood memories through 
-                            a mixture of family photos, home videos and personal childhood photos using analogue techniques 
-                            to re-record and transform archival material. The film transitions between still images and 
-                            slowed-down videos featuring moments of distortion and instability throughout, reflecting how 
-                            memory is often uncertain and changes over time. The soundtrack features a recording of the 
-                            filmmaker playing piano as a child; the audio is slowed and distorted to create something that 
-                            sounds familiar yet feels distant at the same time. Instead of trying to interpret the past, 
-                            this documentary acknowledges memories as emotional, inconsistent, and shaped by what remains 
-                            recorded.`,
+                title: "accretion",
+                author: "Charlotte Payne",
+                description: `This piece is inspired by the defi nition of the word “accretion,” defi ned as the process 
+                            of growth or increase, typically by the gradual accumulation of additional layers or matter. 
+                            It follows the encroachment of nature into modern architecture, beginning with isolated 
+                            buildings, moving through moments of coexistence between nature and the built environment, 
+                            and culminating in a pure focus on nature. The ambience of car noises gradually giving way 
+                            to birds chirping and ambient music reinforces this micronarrative and enhances the atmosphere. 
+                            Ultimately, this piece exemplifi es the emptiness of man-made structure without the presence 
+                            of greenery, illustrating that humanity cannot exist without it.`,
                 xButton: "20%",
                 yButton: "21%",
                 wButton: "37%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/05.png"
             },
 
             // 6
             {
-                title: "Crossing the Alexandra Bridge",
-                author: "Stephen Bowles",
-                description: `Growing up, I always hated driving through the flat, mundane streets of the Canadian suburbs. 
-                            Everything is just so far apart, it feels impossible to walk anywhere. Every plot of land is 
-                            filled by a plaza with an oversized parking lot. Every store is corporate and sells the same 
-                            thing for varying, hiked up prices. I was so sick of it. When I turned 18, I decided to escape 
-                            the excruciatingly tedious life of the Canadian suburbs and take a two week long trip to the 
-                            city of Ottawa. Crossing the Alexandra Bridge is about the excitement and mystique of falling 
-                            in love with a place and earning your independence through the questionable decisions you make 
-                            during your adolescence.`,
+                title: "The Peek 觀𥦬日記",
+                author: "Charlotte Wong",
+                description: `The Peek explores the transition from human presence to absence through window-framed images. Images 
+                            transfer from lively scenes with people to empty classrooms and hallways, as well as spaces that suggest 
+                            recent activity despite their emptiness. Each strong note switches to a new image, reflecting how the 
+                            human eye blinks and refocuses. Combine both visual and sound together reflect on absence, and the 
+                            lingering traces of everyday life.`,
                 xButton: "60%",
                 yButton: "21%",
                 wButton: "30%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/06.png"
             },
 
             // 7
             {
-                title: "GO SPORTS",
-                author: "Matteo Reda",
-                description: `This documentary is a key to my life, and wat I go through on a day-to-day basis. As cliche as 
-                            it sounds, I've been around sports my entire life and I don't regret one bit of it. Every relationship, 
-                            memory, and excitement in my life I feel comes from sports. My family all played sports growing up 
-                            and it is fundamental for physical activity and mental strength. This passion of mine will never end 
-                            and is part of my identity and personality. Every day I go about my life working out, playing hockey, 
-                            watching football, spending time with my teammates... The glue to my life is sports. I don't think that 
-                            when it's all said and done and I am done playing competitive sports, I would disregard everything that 
-                            has made me who I am. Whether it was when I was 3 or 20, there was never any bit of regret to play any 
-                            sport with anyone at anytime.
-                            So here’s to sports! Go Sports!`,
+                title: "Frozen in Time",
+                author: "Christian F Downey-Acevedo",
+                description: `This composition captures a dog, observing a still forest. Each transition between photographs is representative 
+                            of the dog blinking and looking around. Within this still forest, the dog observes mannequin like tree branches, 
+                            that once held life and posture. The dog comes across a particular branch that has the texture of platinum, a boat 
+                            sailing on grassy waves, and a smiling tree, observing the dog’s curiosity.`,
                 xButton: "16%",
                 yButton: "29%",
                 wButton: "40%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/07.png"
             },
 
             // 8
             {
-                title: "A Glimpse Through my Eyes",
-                author: "insomniak03",
-                description: `For this video I wanted to try and emulate the feeling
-                             that you get flipping through a photo album. By incorporating fast
-                             moving images with slower, calmer video elements I intended to
-                             imitate the highs and lows of everyday life and how quickly
-                             memories can be forgotten.`,
+                title: "Caught in Passing",
+                author: "Damian Boukhers",
+                description: `Caught in Passing is a black-and-white picture film that depicts an underground campus parking garage 
+                             as a shared space for architectural and human presence. The sequence balances moments of movement and 
+                             stillness using repetition, low light, and restrained pace, giving the subject and surroundings equal 
+                             visual weight. Meaning comes gradually from spatial rhythm and subtle visual transitions rather than 
+                             narrative action.`,
                 xButton: "60%",
                 yButton: "29%",
                 wButton: "30%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/08.png"
             },
 
              // 9
             {
-                title: "Memories Fading into the Abyss",
-                author: "Lorenzo Rillo",
-                description: `Memories Fading into the Abyss explores childhood memory as something 
-                            that dissolves over time rather than remaining present. Through analogue 
-                            material transformation, such as childhood VHS tapes re-recorded off of 
-                            a TV screen. Also photographs re-recorded using scanning, and then burned. 
-                            Creating physical alteration, allowing parts of the images to become badly 
-                            damaged like fragmented memories. The work reflects on how early life is 
-                            often remembered through photos and videos instead of direct experience, 
-                            creating distance between lived memory and recorded evidence. The audience 
-                            is invited to experience memory not as a clear record, but as something 
-                            dissolving into absence.`,
+                title: "Blown Fuse",
+                author: "Emaris Cruz-Sanchez",
+                description: `Blown Fuse is a piece about the attempts to respark imagination during phases of boredom or burnout, 
+                            but to no avail. The image above is representative of the slow passage of time while capturing the empty 
+                            hollow feeling one gets when looking at themselves for inspiration. The subject was asked to create puppet 
+                            figures and the photos were taken as they slowly gave up and ran out of ideas. Gently complementing the 
+                            series of photos is the sound of a damaged fluorescent capacitor repeatedly failing to turn on the light.`,
                 xButton: "20%",
                 yButton: "37%",
                 wButton: "37%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/09.png"
             },
 
             // 10
@@ -184,7 +171,8 @@ const App = {
                 xButton: "60%",
                 yButton: "37%",
                 wButton: "30%",
-                hButton: "5%"
+                hButton: "5%",
+                thumbnail: "imgs/10.png"
             },
 
 
