f4b8ac8bed7e6f3ec724a8a8fe26344deca267f9
Jessica A. Rodriguez <44589009+jac307@users.noreply.github.com>
2026-02-11 20:54:16 -0500

    Update app.js

--- zines/app.js
+++ zines/app.js
@@ -10,9 +10,14 @@ const App = {
         const projects = [
             // 1
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Nesting",
+                author: "Mara Sanders",
+                description: `Watercolour on paper, fabric, thread, graphite on paper / 13" x 2" x 10" 
+                            ——— Brief description of the time-based narrative: To reflect on the idea of a 'time based' narrative, 
+                            I wanted to look  at my own artistic practice and use of media. Knitting, sewing, and watercolour 
+                            paintings are what began my art practice early in my childhood, and have been constant materials 
+                            I have reached for in the years since. By integrating these mediums, I wanted to illustrate how I am 
+                            made up of these artistic expressions, and how I have built a protective space in art for myself.`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -23,9 +28,11 @@ const App = {
             // 2
 
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Withered",
+                author: "Bronte Mayot",
+                description: `Multimedia work containing dried-up rose metals, digital illustrations on watercolour paper / 8.5 x 11”
+                            ——— This work is inspired by the concepts of decay and death, through the imagery of flowers as 
+                            storytellers to symbolize the passage of time.`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -36,9 +43,10 @@ const App = {
 
             // 3
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Bedtime",
+                author: "Tristan Zadvorny",
+                description: `I used coloured pencils and acrylic wash on regular card paper / 4.5 x 6”
+                            ——— My work is inspired by the novelty of going to bed, and sleeping`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -49,9 +57,10 @@ const App = {
 
             // 4
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Blue Gill’s Descent - Journal #2",
+                author: "Rowan van Oosten",
+                description: `Ink on paper / 15cm x 25cm 
+                            ——— My work is inspired by handmade research journals. It explores the fears and feelings of a scientist, Blue Gills throughout a underwater ecological disaster with a supernatural twist.`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -63,9 +72,11 @@ const App = {
 
             // 5
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "You need to wake up Kevin",
+                author: "Kevin Wu",
+                description: `I used the color markers to color it. And then made a book by folding the paper. Then put it in the box. 
+                            ——— My work was inspired by the fact that I slept for a whole day last week and realized how quickly time passes. I need to cherish time . I created this 
+                            book made up of ancient Chinese clocks and Western clock numbers.`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -76,9 +87,10 @@ const App = {
 
             // 6
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Swampy Ditches",
+                author: "Maia Venturelli Golba",
+                description: `Harvesting in the 905 Watercolour/Acrylic paint and Photo collage / 59 X 11 
+                            ——— Inspired by the real harvest records of a local Hamilton farm, Harvesting in the 905 divides time through different vegetables harvested in seasons.”`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
@@ -89,9 +101,10 @@ const App = {
 
             // 7
             {
-                title: "",
-                author: "",
-                description: ``,
+                title: "Leave my body behind, I'll take the rest",
+                author: "Charlotte Heron",
+                description: `Paper, Epson printer, Brother cut and scan / 5.5 in x 9.5 in x 0.5 in 
+                            ——— A non linear story about an ever changing relationship to self, gender and womanhood--- it gives just as much as it takes.`,
                 xButton: "28%",
                 yButton: "5%",
                 wButton: "30%",
